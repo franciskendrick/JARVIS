3e23249c26cf8dffe2a9fc349310e4186c6e4c0d
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-08-24 14:19:06 +0800

    Added general's on command error

--- src/cogs/general.py
+++ src/cogs/general.py
@@ -18,6 +18,10 @@ class General(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+        # Errors
+        with open(f"{resources_path}/errors.json") as json_file:
+            self.errors = json.load(json_file)
+
     @commands.Cog.listener()
     async def on_ready(self):
         print("{0.user} has logged in.".format(self.bot))
@@ -25,7 +29,18 @@ class General(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        pass
+        if isinstance(error, commands.CommandNotFound):
+            messages_list = self.errors["CommandNotFound"]
+
+        # Get message from messages list
+        message = random.choice(messages_list)
+
+        # Replace message's key words
+        message = message.replace("__user__", f"<@{ctx.author.id}>")
+        message = message.replace("__creator__", f"<@{CREATOR_ID}>")
+
+        # Send
+        await ctx.send(message)
 
 
 def setup(bot):
